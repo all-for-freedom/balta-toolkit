500cb36000dace5ed846ea727a2112d28e808e51
all-for-freedom <yangshen@hunnu.edu.cn>
2025-10-30 21:17:45 +0800

    初始化一个几乎空的项目

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+/*
+ * 北太天元(Baltamatica)t-SNE 插件主文件。
+ * 适用于 v4.1 版本的北太天元。
+ *
+ * ngrid 函数:
+ *   [dm, ds] = ngrid(A, B)
+ */
+
+#include "bex/bex.hpp"
+
+using namespace baltam;
+#define PLUGIN_NAME "self_use"
+
+void isPositiveDefinite(int, bxArray*[], int, const bxArray*[]);
+
+static const char * isPositiveDefinite_help = R"(
+    这是一个模板文件
+)";
+
+/**
+ * @brief bxPluginInit 的定义
+ *
+ * bxPluginInit(args...) 会在调用 load_plugin(name, args...) 时调用,目的是
+ * 进行一些初始化工作。
+ *  
+ * 目前还不知道需要做什么初始化工作
+ */
+
+int bxPluginInit(int, const bxArray*[]){ return 0;}
+
+
+/**
+ * @brief bxPluginFini 的定义
+ *
+ * bxPluginFini() 会在调用 unload_plugin() 时调用。目的是进行一些析构的工作。
+ *
+ * 这里因为无需特别的析构,所以定义为空函数。
+ */
+
+int bxPluginFini(){ return 0; }
+
+
+/**
+ * @brief bxPluginFunctions 的定义
+ *
+ * bxPluginFunctions 返回一个 bexfun_info_t * 的指针,指向函数列表的第一个元素。
+ * 列表中最后一个元素的 name 标签必须为空字符串常量。
+ * 函数列表可以以静态变量的方式定义在 .cpp 或 .c 中。
+ *
+ */
+static bexfun_info_t flist[] = {
+    {"isPositiveDefinite", isPositiveDefinite, isPositiveDefinite_help},
+    {"", nullptr, nullptr},
+};
+
+bexfun_info_t * bxPluginFunctions(){
+    return flist;
+}
+
+/**
+ * @brief 以下是插件函数的实际定义。
+ * 
+ */
+
+void isPositiveDefinite(int nlhs, bxArray *plhs[], int nrhs, const bxArray *prhs[]){
+    
+}
